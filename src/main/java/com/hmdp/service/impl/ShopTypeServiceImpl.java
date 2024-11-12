b213e402c499ef12d2dac269233db2a4bbe75089
han <2907880087@qq.com>
2024-11-12 17:51:06 +0800

    typeList 缓存

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_TYPE_LIST;
 
 /**
  * <p>
@@ -16,5 +26,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryList() {
+        String key= CACHE_TYPE_LIST;
+        String typeJson = stringRedisTemplate.opsForValue().get(key);
+        if(StrUtil.isNotBlank(typeJson)){
+            List<ShopType> shopTypeList = JSONUtil.toList(typeJson, ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shopTypeList));
+        return Result.ok(shopTypeList);
+    }
 }
