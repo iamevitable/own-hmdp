0547dd302834ab0d8c35bf2f3868678e734ae59b
han <2907880087@qq.com>
2024-11-16 11:35:08 +0800

    实现秒杀下单功能

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -41,10 +41,10 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryById(Long id) {
 //        逻辑过期解决缓存击穿问题
-        Shop shop = cacheClient
-                .queryWithLogicalExpire(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+//        Shop shop = cacheClient
+//                .queryWithLogicalExpire(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
 //        解决缓存穿透
-//        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
 //        互斥锁解决缓存击穿问题
 //        Shop shop = queryWithMutex( id );
 //        逻辑过期解决缓存击穿问题
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -15,6 +24,46 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Transactional
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
-
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //1.查询优惠卷
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        //2.判断优惠卷是否开始和结束
+        if(voucher.getBeginTime().isAfter(LocalDateTime.now())){
+            return Result.fail("秒杀未开始");
+        }
+        if(voucher.getEndTime().isBefore(LocalDateTime.now())){
+            return Result.fail("秒杀已结束");
+        }
+        //3.判断优惠卷是否已经被抢完
+        if(voucher.getStock() < 1){
+            return Result.fail("优惠卷已被抢完");
+        }
+        //4.扣减优惠卷库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).update();
+        if(!success){
+            return Result.fail("扣减优惠卷库存失败");
+        }
+        //5.生成订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        //订单号生成规则：时间戳+用户id+随机数
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        //用户id
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+        //优惠卷id
+        voucherOrder.setVoucherId(voucherId);
+        //6.返回订单号
+        save(voucherOrder);
+        return Result.ok(orderId);
+    }
 }
