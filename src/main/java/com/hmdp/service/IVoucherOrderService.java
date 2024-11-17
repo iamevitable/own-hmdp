c1badc98eccd9b420051b4004af7229556b3dd33
han <2907880087@qq.com>
2024-11-17 19:55:13 +0800

    实现一人一单

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -15,4 +15,5 @@ import com.hmdp.entity.VoucherOrder;
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
     Result seckillVoucher(Long voucherId);
+    Result createVoucherOrder(Long orderId);
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,8 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,26 +47,40 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if(voucher.getStock() < 1){
             return Result.fail("优惠卷已被抢完");
         }
-        //4.扣减优惠卷库存
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            //这里如果直接返回createVoucherOrder(voucherId) 即调用了this方法 又因为this方法没有使用动态代理
+            //所以无法提交事务 所以要使用AopContext创建一个动态代理出来 这样才能提交事务
+            IVoucherOrderService proxy =(IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        Long userId = UserHolder.getUser().getId();
+        long count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if (count > 0) {
+            return Result.fail("you have bought it");
+        }
+        //5.生成订单
+        VoucherOrder voucherOrder = new VoucherOrder();
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
                 .eq("voucher_id", voucherId).gt("stock", 0)
                 .update();
-        if(!success){
+        if (!success) {
             return Result.fail("扣减优惠卷库存失败");
         }
-        //5.生成订单
-        VoucherOrder voucherOrder = new VoucherOrder();
         //订单号生成规则：时间戳+用户id+随机数
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
         //用户id
-        Long userId = UserHolder.getUser().getId();
         voucherOrder.setUserId(userId);
         //优惠卷id
         voucherOrder.setVoucherId(voucherId);
         //6.返回订单号
         save(voucherOrder);
+
         return Result.ok(orderId);
     }
 }
